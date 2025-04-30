1052fddbd8c393b1fd429471085cd27c706d24fe
Viktor Shterev <viktor.shterev55@gmail.com>
2025-04-30 12:48:17 +0300

    adding package rest controller

---
+++ src/main/java/my/projects/lockersystempackagemicroservice/web/PackageRestController.java
@@ -0,0 +1,31 @@
+package my.projects.lockersystempackagemicroservice.web;
+
+import my.projects.lockersystempackagemicroservice.dto.CreatePackageDTO;
+import my.projects.lockersystempackagemicroservice.dto.ReceivePackageDTO;
+import my.projects.lockersystempackagemicroservice.service.PackageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PackageRestController {
+
+    private final PackageService packageService;
+
+    public PackageRestController(PackageService packageService) {
+        this.packageService = packageService;
+    }
+
+    @PostMapping("/create-package")
+    public ResponseEntity<Boolean> createPackage(@RequestBody CreatePackageDTO createPackageDTO) {
+        boolean isSuccessful = this.packageService.createPackage(createPackageDTO);
+        return ResponseEntity.ok(isSuccessful);
+    }
+
+    @PostMapping("/receive-package")
+    public ResponseEntity<Boolean> receivePackage(@RequestBody ReceivePackageDTO receivePackageDTO) {
+        boolean isSuccessful = this.packageService.receivePackage(receivePackageDTO);
+        return ResponseEntity.ok(isSuccessful);
+    }
+}
