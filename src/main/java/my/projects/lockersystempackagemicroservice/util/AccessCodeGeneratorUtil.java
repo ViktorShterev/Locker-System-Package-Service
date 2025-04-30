b18069da413568d84967be9e96a70f18d56fe541
Viktor Shterev <viktor.shterev55@gmail.com>
2025-04-30 12:51:18 +0300

    adding access code generator

---
+++ src/main/java/my/projects/lockersystempackagemicroservice/util/AccessCodeGeneratorUtil.java
@@ -0,0 +1,21 @@
+package my.projects.lockersystempackagemicroservice.util;
+
+import java.security.SecureRandom;
+
+public class AccessCodeGeneratorUtil {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int CODE_LENGTH = 6;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateAlphanumericCode() {
+        StringBuilder code = new StringBuilder(CODE_LENGTH);
+
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            code.append(CHARACTERS.charAt(index));
+        }
+
+        return code.toString();
+    }
+}
+
