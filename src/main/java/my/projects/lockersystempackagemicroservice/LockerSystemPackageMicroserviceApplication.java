4ee9902feddc8bed204dc4c250148ceb18d2bcaf
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-01 14:57:31 +0300

    adding pom and application.yaml

---
+++ src/main/java/my/projects/lockersystempackagemicroservice/LockerSystemPackageMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package my.projects.lockersystempackagemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerSystemPackageMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockerSystemPackageMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/projects/lockersystempackagemicroservice/LockerSystemPackageMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.projects.lockersystempackagemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerSystemPackageMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
