ef51aa561bd62a91580a1676cc6c3711cb33aff9
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-01 14:56:28 +0300

    fixes

--- src/main/java/my/projects/lockersystempackagemicroservice/web/PackageRestController.java
+++ src/main/java/my/projects/lockersystempackagemicroservice/web/PackageRestController.java
@@ -6,9 +6,11 @@ import my.projects.lockersystempackagemicroservice.service.PackageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("http://localhost:8082/packages")
 public class PackageRestController {
 
     private final PackageService packageService;
