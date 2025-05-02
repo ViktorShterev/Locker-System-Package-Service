33d68c102664f2a697dc1c7bbbca756587e781ae
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-02 11:13:31 +0300

    upgrading controller

--- src/main/java/my/projects/lockersystempackagemicroservice/web/PackageRestController.java
+++ src/main/java/my/projects/lockersystempackagemicroservice/web/PackageRestController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
-@RequestMapping("http://localhost:8082/packages")
+@RestController
+@RequestMapping("/packages")
 public class PackageRestController {
 
     private final PackageService packageService;
