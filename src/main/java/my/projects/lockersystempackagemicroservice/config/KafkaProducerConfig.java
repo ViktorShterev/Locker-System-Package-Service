5cbd6efb34e925d0d3abcbb8a3bf70cd4cebf95a
Viktor Shterev <viktor.shterev55@gmail.com>
2025-05-14 09:30:45 +0300

    add kafka config

---
+++ src/main/java/my/projects/lockersystempackagemicroservice/config/KafkaProducerConfig.java
@@ -0,0 +1,37 @@
+package my.projects.lockersystempackagemicroservice.config;
+
+import my.projects.lockersystempackagemicroservice.dto.kafka.PackageEventDTO;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, PackageEventDTO> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        // Prevent adding type info headers
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, PackageEventDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
+
